d4da31d37d1f1e952e6b40678de1dee99ec0a495
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-06 16:17:34 -0500

    ex2

--- js_lab.js
+++ js_lab.js
@@ -4,18 +4,54 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
-function pigIt(str) {
-    const punctuation = ['!', ',', '.','?']
-    let newArr = str.split(" ")
-    return newArr.map(item => {
-        if (item.length === 1 ){
-            if (punctuation.includes(item)) return item
-            else return item + 'ay'
-        } else {
-            let newI = item.slice(1) + item[0] + 'ay'
-            return newI
+// function pigIt(str) {
+//     const punctuation = ['!', ',', '.','?']
+//     let newArr = str.split(" ")
+//     return newArr.map(item => {
+//         if (item.length === 1 ){
+//             if (punctuation.includes(item)) return item
+//             else return item + 'ay'
+//         } else {
+//             let newI = item.slice(1) + item[0] + 'ay'
+//             return newI
+//         }
+//     }).join(" ")
+// }
+
+// console.log(pigIt("Hello world o ? !"));
+
+
+//**************************************************** */
+// EX2
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+// SOLUTION
+function duplicateCount(text) {
+    let newArr = text.split("")
+    let count = 0
+    let newObj = newArr.reduce((prev, char) => {
+        if (prev[char.toLowerCase()]) {
+            prev[char.toLowerCase()] += 1
         }
-    }).join(" ")
+        else prev[char.toLowerCase()] = 1
+        return prev
+    }, {})
+    Object.keys(newObj).forEach(key => {
+        if (newObj[key] > 1) {
+            count += 1
+        }
+    }
+    )
+    return count
 }
 
-console.log(pigIt("Hello world o ? !"));
--- py_lab.py
+++ py_lab.py
@@ -4,17 +4,42 @@
 # pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 # pigIt('Hello world !');     // elloHay orldway !
 
+# SOLUTION
+# def pigIt(str):
+#     punctuation = ["!", ",", ".", "?"]
+#     new_lst = str.split(" ")
+#     res = []
+#     for item in new_lst:
+#         if len(item) == 1:
+#             res.append(item) if item in punctuation else res.append(item + "ay")
+#         else:
+#             res.append(item[1:] + item[0] + "ay")
+#     return " ".join(res)
 
-def pigIt(str):
-    punctuation = ["!", ",", ".", "?"]
-    new_lst = str.split(" ")
-    res = []
-    for item in new_lst:
-        if len(item) == 1:
-            res.append(item) if item in punctuation else res.append(item + "ay")
-        else:
-            res.append(item[1:] + item[0] + "ay")
-    return " ".join(res)
-   
-print(pigIt("Pig latin is cool"))
-print(pigIt("Hello world !"))
+# print(pigIt("Pig latin is cool"))
+# print(pigIt("Hello world !"))
+
+# *********************************************
+# EX2
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+
+# SOLUTION
+def duplicatedCount(text):
+    new_dict = {item: text.lower().count(item) for item in text}
+    count = 0
+    for v in new_dict.values():
+        if v > 1:
+            count += 1
+    return count
+
