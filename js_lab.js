f5ec7212f37336b8b352428d5b5a332f4d42f7ed
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-13 16:43:58 -0500

    ex11

--- js_lab.js
+++ js_lab.js
@@ -215,3 +215,43 @@
 //     else return (BigInt(a) + BigInt(b)).toString()
 // }
 // console.log(sumString('712569312664357328695151392', '8100824045303269669937'))
+
+
+// ***************** ex11
+// Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+
+// For example:
+
+//  // should return true
+// [ 1, 1, 1 ].sameStructureAs( [ 2, 2, 2 ] );          
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ 2, [ 2, 2 ] ] );  
+
+//  // should return false 
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2, 2 ], 2 ] );  
+// [ 1, [ 1, 1 ] ].sameStructureAs( [ [ 2 ], 2 ] );  
+
+// // should return true
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ [ ], [ ] ] ] ); 
+
+// // should return false
+// [ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );
+
+// Array.prototype.sameStructureAs = function (other) {
+//     //check the other is array or not, and check length, if not array or the length is not the same, return false
+//     if (!Array.isArray(other) || this.length !== other.length) {
+//         return false
+//     }
+//     for (let i = 0; i < this.length; i++) {
+//         if (typeof (this[i]) !== typeof (other[i])) {
+//             return false
+//         }
+//         // Using Recursion to test each element in this[i] and other[i]
+//         if (Array.isArray(this[i]) && !this[i].sameStructureAs(other[i])) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+// console.log([1, 1, 1].sameStructureAs([2, 2, 2]));
+// console.log([[1, 1], 1].sameStructureAs([2, [2, 2]]));
+
